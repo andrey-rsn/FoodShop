3d0453907184b8867358125ff14b634db2a75003
andrey-rsn <korowai98.ag@gmail.com>
2021-12-15 00:03:22 +0300

    CreatedProductApiDb

---
+++ FoodShop.Services.ProductAPI/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FoodShop.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodShop.Services.ProductAPI.DBContext
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base (options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
