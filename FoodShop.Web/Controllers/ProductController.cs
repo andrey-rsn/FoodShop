3620385bb64e9fac898bbd2e540cda9e4d293ee5
andrey-rsn <korowai98.ag@gmail.com>
2021-12-18 19:33:05 +0300

    AddedProductsCreateView

---
+++ FoodShop.Web/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using FoodShop.Web.Models;
+using FoodShop.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FoodShop.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDTO> list = new List<ProductDTO>();
+            var response = await _productService.GetAllProductsAsync<ResponseDTO>();
+            if(response!= null && response.IsSuccess)
+            {
+                list=JsonConvert.DeserializeObject<List<ProductDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductСreate(ProductDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProductAsync<ResponseDTO>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(model);
+
+        }
+    }
+}
